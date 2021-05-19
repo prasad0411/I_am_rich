906ab95c47331aedb5ddc92d1e968eb892f18be8
prasad kanade <prasadckanade@gmail.com>
2021-05-19 19:51:49 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:flutter_application/main.dart';
+
+void main() 
+{
+  runApp(
+   MaterialApp( 
+     debugShowCheckedModeBanner: false,
+      home: Scaffold( 
+        appBar: AppBar(
+          title: Text("I Am Rich"),
+          backgroundColor: Colors.black
+        ),
+         backgroundColor: Colors.purple,
+         body: Center(
+           child: 
+             Image(
+              image: NetworkImage('https://flutter.dev/images/flutter-logo-sharing.png')
+              ),
+              ),
+          ),
+      ),
+  );
+}
+
+
