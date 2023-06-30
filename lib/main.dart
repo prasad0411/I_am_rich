bdb44310835ae066e75e0eb3f02c3334fd6e7065
Prasad Kanade <65277692+prasad0411@users.noreply.github.com>
2023-06-30 19:51:01 +0530

    Added comments

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,22 @@ import 'package:flutter/material.dart';
 // ignore: unused_import
 import 'package:flutter_application/main.dart';
 
-void main() 
-{
+void main() {
   runApp(
-   MaterialApp( 
-     debugShowCheckedModeBanner: false,
-      home: Scaffold( 
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
         appBar: AppBar(
           title: Text("I Am Rich"),
-          backgroundColor: Colors.black
+          backgroundColor: Colors.black,
         ),
-         backgroundColor: Colors.purple,
-         body: Center(
-           child: 
-             Image(
-              image: NetworkImage('https://flutter.dev/images/flutter-logo-sharing.png')
-              ),
-              ),
+        backgroundColor: Colors.purple,
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://flutter.dev/images/flutter-logo-sharing.png'),
           ),
+        ),
       ),
+    ),
   );
 }
-
-
